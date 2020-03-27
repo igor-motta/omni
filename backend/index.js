a5ece6d6ec8ed9ffd829c06727edeb538bca4988
igro <igor.fmotta@gmail.com>
2020-03-26 23:13:36 -0300

    testing requests

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World!" });
+app.post("/plants", (request, response) => {
+  const params = request.query;
+  console.log(params);
+  return response.json({
+    message: "Hello World!"
+  });
 });
 
 app.listen(8801);
